f7ced4b8e01e730a0d49f8b3f549d6132157d07a
Mll0032 <mllawson18@gmail.com>
2024-10-10 18:57:22 -0500

    Create the JobTest.java code testSettingJobId() making sure each job has a unique Id. testJobConstructorSetsAllFields() making sure to use assertEquals() and assertTrue() to make sure the constructor assigns both the class and value to each field. testJobsforEquality check if equals returns false. since in our case it should only return false since all our jobs have a unique Id it should never return true.

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,54 @@
 package org.launchcode.techjobs.oo;
 
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class JobTest {
     //TODO: Create your unit tests here
+@Test
+public void testSettingJobId() {
+    Job job1 = new Job();
+    Job job2 = new Job();
+    assertNotEquals(job1.getId(), job2.getId());
+}
+
+@Test
+public void testJobConstructorSetsAllFields(){
+    Job job = new Job("Product tester",
+            new Employer("ACME"),
+            new Location("Desert"),
+            new PositionType("Quality control"),
+            new CoreCompetency("Persistence"));
+
+    assertEquals("Product tester", job.getName());
+    assertTrue(job.getName() instanceof String);
+    assertEquals("ACME", job.getEmployer().getValue());
+    assertTrue(job.getEmployer() instanceof Employer);
+    assertEquals("Desert", job.getLocation().getValue());
+    assertTrue(job.getLocation() instanceof Location);
+    assertEquals("Quality control", job.getPositionType().getValue());
+    assertTrue(job.getPositionType() instanceof PositionType);
+    assertEquals("Persistence", job.getCoreCompetency().getValue());
+    assertTrue(job.getCoreCompetency() instanceof CoreCompetency);
+
+}
+
+@Test
+public void testJobsForEquality() {
+    Job job1 = new Job("Product tester",
+            new Employer("ACME"),
+            new Location("Desert"),
+            new PositionType("Quality control"),
+            new CoreCompetency("Persistence"));
+    Job job2 = new Job("Product tester",
+            new Employer("ACME"),
+            new Location("Desert"),
+            new PositionType("Quality control"),
+            new CoreCompetency("Persistence"));
+
+    assertFalse(job1.equals(job2));
+}
+
 }
